2d4f69c8ff5a39b12fe55f8c3ab55a4219e416f1
Dudualuizio17 <88692610+Dudualuizio17@users.noreply.github.com>
2022-11-25 19:27:27 -0300

    CRUD Cliente Commpleto

--- projectFront/webapp/controller/View2.controller.js
+++ projectFront/webapp/controller/View2.controller.js
@@ -2,29 +2,20 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/ui/core/mvc/Controller",
 	"sap/base/Log",
-	"sap/ui/model/json/JSONModel"
+	"sap/ui/model/json/JSONModel",
+	"sap/m/Text",
+	"sap/m/Button"
 
-], function (MessageToast, Controller, Log, JSONModel) {
+], function (MessageToast, Controller, Log, JSONModel, Text, Button) {
 	"use strict";
 
 	return Controller.extend("projectbetelgas.controller.View2", {
 
 		onInit() {
 
-			var oDataSetModel = {
+			window.modoFormularioCliente = "cadastro"
 
-
-				aNome: [
-					{
-						nome: "Aluizio", sobrenome: "Antonio", telefone: "11 952096999", endereço: "Rua Curitiba, 48", cep: "07261-070", tempoDeConsumo: "60 dias"
-					},
-					{
-						nome: "José", sobrenome: "Silva", telefone: "11 952059908", endereço: "Rua Guapo, 85", cep: "07252-090", tempoDeConsumo: "45 dias"
-					}
-				]
-
-
-			}
+			this.selectClientTable();
 
 			var oDataSetModelProd = {
 
@@ -72,21 +63,373 @@ sap.ui.define([
 				price: 17600
 			}
 
-			var oModel = new JSONModel(oDataSetModel);
 			var oModelProd = new JSONModel(oDataSetModelProd);
 			var oModelVenda = new JSONModel(oDataSetModelVenda);
 			var oModelEstoque = new JSONModel(oDataSetModelEstoque);
 			var oModelCaixa = new JSONModel(oDataSetModelCaixa);
 
-			this.getView().setModel(oModel, "modelCliente");
+
 			this.getView().setModel(oModelProd, "modelProd");
 			this.getView().setModel(oModelVenda, "modelVenda");
 			this.getView().setModel(oModelEstoque, "modelEstoque");
 			this.getView().setModel(oModelCaixa, "modelCaixa");
 		},
+		//Clientes
+		selectClientTable() {
+			var that = this;
+
+			//Selecionando todos os Clientes
+			$.ajax({
+				type: "GET",
+				url: "http://localhost:3000/clientes/client",
+
+				success: function (oResponse) {
+					debugger
+					var oModel = new JSONModel({
+						aNome: oResponse.response
+					});
+
+					that.getView().setModel(oModel, "modelCliente");
+				},
+				error: function (oResponse) {
+					debugger
+				}
+			});
+
+		},
+
+		clearFormInput: function (oParam) {
+			debugger
+			oParam.oInputNome.setValue("")
+			oParam.oInputSobrenome.setValue("")
+			oParam.oInputTelefone.setValue("")
+			oParam.oInputWhatsapp.setValue("")
+			oParam.oInputEndereco.setValue("")
+			oParam.oInputCep.setValue("")
+
+		},
+
+		onPress: function (evt) {
+			debugger
+			var oInputNome = this.getView().byId("input-a")
+			var oInputSobrenome = this.getView().byId("input-b")
+			var oInputTelefone = this.getView().byId("input-c")
+			var oInputWhatsapp = this.getView().byId("input-d")
+			var oInputEndereco = this.getView().byId("input-e")
+			var oInputCep = this.getView().byId("input-f")
+
+			var oCreateData = new Date();
+
+			var that = this;
+
+			
+
+			if (window.modoFormularioCliente == "cadastro") {
+				var oPostClient = {
+
+					"name_cli": oInputNome.getValue(),
+					"surname_cli": oInputSobrenome.getValue(),
+					"fone1_cli": oInputTelefone.getValue(),
+					"fone2_cli": oInputWhatsapp.getValue(),
+					"adress_cli": oInputEndereco.getValue(),
+					"cep_cli": oInputCep.getValue(),
+					"createdate_cli": oCreateData.toISOString().slice(0, 10),
+					"status_cli": 1
+				}
+				// Inserindo um novo cliente
+				$.ajax({
+					type: "POST",
+					url: "http://localhost:3000/clientes/client",
+					data: oPostClient,
+
+					success: function (oResponse) {
+						debugger
+						var oParam = {
+							oInputNome: oInputNome,
+							oInputSobrenome: oInputSobrenome,
+							oInputTelefone: oInputTelefone,
+							oInputWhatsapp: oInputWhatsapp,
+							oInputEndereco: oInputEndereco,
+							oInputCep: oInputCep
+						}
+
+						that.clearFormInput(oParam);
+
+						that.selectClientTable();
+
+						MessageToast.show("Cadastrado com Sucesso");
+					},
+					error: function (oResponse) {
+						debugger
+					}
+
+				});
+
+			}
+
+			if (window.modoFormularioCliente == "edit") {
+
+				var oUpdateCliente = that.getView().getModel("oModelEditCli").getData();
+
+
+				$.ajax({
+					type: "PUT",
+					url: "http://localhost:3000/clientes",
+					data: oUpdateCliente,
+
+					success: function (oResponse) {
+						debugger
+						var oParam = {
+							oInputNome: oInputNome,
+							oInputSobrenome: oInputSobrenome,
+							oInputTelefone: oInputTelefone,
+							oInputWhatsapp: oInputWhatsapp,
+							oInputEndereco: oInputEndereco,
+							oInputCep: oInputCep
+						}
+
+						that.clearFormInput(oParam);
+
+						that.selectClientTable();
+
+						window.modoFormularioCliente = "cadastro";
+
+						that.getView().byId("btnCliente").setText("Cadastrar")
+
+						MessageToast.show("Atualizado com Sucesso");
+					},
+					error: function (oResponse) {
+						debugger
+					}
+
+				});
+
+			}
+
+
+
+
+
+
+
+			// Selecionando por id
+			/*$.ajax({
+				type: "GET",
+				url: "http://localhost:3000/clientes/1",
+
+				success: function (oResponse) {
+					debugger
+
+				},
+				error: function (oResponse) {
+					debugger
+				}
+			});*/
+
+		},
+
+
+		onPressEdit: function (oEvent) {
+			debugger
+		var	that = this;
+			//Pegar o elemento que foi clicado, guardar na variavel esse objeto
+			//criar um model com a variavel criada, mandando como parametro de definição
+			//Setar o model na view com o alias que já está definido (oModelEditCli)
+			//criar uma variavel global para decidir se é modo cadastro ou modo edição
+			var oLinhaClicada = oEvent.getSource().getBindingContext("modelCliente").getProperty()
+			var oModel = new JSONModel(oLinhaClicada);
+
+			that.getView().setModel(oModel, "oModelEditCli");
+
+			that.getView().byId("btnCliente").setText("Atualizar");
+
+			window.modoFormularioCliente = "edit"
+
+			that.getView().byId("idIconTabBarCliente").setSelectedKey(0)
+
+
+
+			// Alterando um cliente
+			/*$.ajax({
+				type: "PUT",
+				url: "http://localhost:3000/clientes",
+				data: oPostClient,
+
+				success: function (oResponse) {
+					debugger
+				},
+				error: function (oResponse) {
+					debugger
+				}
+			});*/
+
+		},
+
+
+
+		onPressDelete: function (oEvent) {
+			debugger
+			var that = this;
+			var oLinhaClicada = oEvent.getSource().getBindingContext("modelCliente").getProperty()
+
+
+			//DIALOG/MODAL/POPUP COM BOTAO
+			if (!oDialog) {
+
+				var oDialog = new sap.m.Dialog({
+					//contentHeight: '100%',
+					//contentWidth: '100%',
+					title: 'Excluir Cliente',
+					content: new Text({ text: "Deseja deletar esse registro?" }),
+					beginButton: new Button({
+						text: 'Cancelar',
+						type: sap.m.ButtonType.Reject,
+						press: function () {
+							oDialog.close();
+						}
+					}),
+					endButton: new Button({
+						text: 'Confirmar',
+						type: sap.m.ButtonType.Accept,
+						press: function () {
+							oDialog.close();
+							that.deleteSlice(oLinhaClicada.client_id_cli, oLinhaClicada.name_cli);
+						}
+					}),
+					afterClose: function () {
+						oDialog.destroy();
+					}
+				});
+
+				that.getView().addDependent(oDialog);
+
+				oDialog.open();
+			} else {
+				oDialog.open();
+			}
+
+		},
+
+		deleteSlice: function (sIddelecao, sNameDelecao) {
+			var that = this;
+
+			var oPayloadDelecao = {
+				"client_id_cli": sIddelecao,
+				"name_cli": sNameDelecao
+			}
+			// Deletando um cliente
+			$.ajax({
+				type: "DELETE",
+				url: "http://localhost:3000/clientes/client",
+				data: oPayloadDelecao,
+
+				success: function (oResponse) {
+					debugger
+					MessageToast.show("Item " + sIddelecao + " " + sNameDelecao + " Deletado com Sucesso");
+					that.selectClientTable();
+				},
+				error: function (oResponse) {
+					debugger
+				}
+			})
+
+		},
+
+		//Produtos
+		onPressPdt: function (evt) {
+			debugger
+			var oInputNomePdt = this.getView().byId("input-h")
+			var oInputDescriptionPdt = this.getView().byId("input-i")
+			var oInputStockControl = this.getView().byId("input-j")
+			var oInputStockMin = this.getView().byId("input-k")
+			var oInputPricePdt = this.getView().byId("input-l")
+
+			var oCreateData = new Date();
+			var oPostProduct = {
+				"name_pdt": oInputNomePdt.getValue(),
+				"description_pdt": oInputDescriptionPdt.getValue(),
+				"stock_control_pdt": oInputStockControl.getValue(),
+				"stock_min_pdt": oInputStockMin.getValue(),
+				"creation_date_pdt": oCreateData.toISOString().slice(0, 10),
+				"price_pdt": oInputPricePdt.getValue(),
+				"status_pdt": 1
+			}
+			// Inserindo um novo Produto
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:3000/produtos/product",
+				data: oPostProduct,
+
+				success: function (oResponse) {
+					debugger
+					oInputNomePdt.setValue("")
+					oInputDescriptionPdt.setValue("")
+					oInputStockControl.setValue("")
+					oInputStockMin.setValue("")
+					oInputPricePdt.setValue("")
+
+				},
+
+				error: function (oResponse) {
+
+				},
+
+			});
+
+
+			MessageToast.show(evt.getSource().getId() + "Pressed");
+		},
+
+		/*onPressSales: function(evt) {
+			debugger
+			var oInputProdSale = this.getView().byId("input-m")
+			var oInputClientSale = this.getView().byId("input-n")
+			var oInputTelefoneSale = this.getView().byId("input-o")
+			var oInputQtdeSale = this.getView().byId("input-p")
+			
+
+			var oCreateData = new Date();
+			var oPostSale = {
+				"name_pdt" : oInputNomePdt.getValue(),
+				"description_pdt": oInputDescriptionPdt.getValue(),
+				"stock_control_pdt": oInputStockControl.getValue(),
+				"stock_min_pdt": oInputStockMin.getValue(),
+				"creation_date_pdt": oCreateData.toISOString().slice(0,10),
+				"price_pdt": oInputPricePdt.getValue(),
+				"status_pdt": 1
+			}
+			// Inserindo um novo Produto
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:3000/produtos/product",
+				data: oPostProduct,
+	
+					success: function (oResponse) {
+						oInputNomePdt.setValue("")
+						oInputDescriptionPdt.setValue("")
+						oInputStockControl.setValue("")
+						oInputStockMin.setValue("")
+						oInputPricePdt.setValue("")
+					},
+
+					error: function (oResponse) {
+						
+					},
+
+				});
+
+
+
+
+			MessageToast.show(evt.getSource().getId() + "Pressed");
+		},*/
+
+		onPressNewProduct: function (evt) {
+			MessageToast.show(evt.getSource().getId() + "Pressed");
+		},
 
 		onPressDarkMode: function (oEvent) {
-			
+
 			var sStatusBotao = oEvent.getSource().getState()
 			if (sStatusBotao == true) {
 				sap.ui.getCore().applyTheme("sap_fiori_3_dark");
@@ -94,10 +437,6 @@ sap.ui.define([
 			else {
 				sap.ui.getCore().applyTheme("sap_horizon");
 			}
-		},
-
-		onPress: function (evt) {
-			MessageToast.show(evt.getSource().getId() + " Pressed");
 		},
 
 		onPressDetailBack: function () {
